4edee5306e4ae657f7bb2821ce70884300f3022e
mangesh vispute <mangesh2003vispute@gmail.com>
2023-11-13 11:57:49 +0530

    added index file

--- djangoauthapi1/djangoauthapi1/settings.py
+++ djangoauthapi1/djangoauthapi1/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'djangoauthapi1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,8 +135,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- djangoauthapi1/djangoauthapi1/urls.py
+++ djangoauthapi1/djangoauthapi1/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
-
+from .import views
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -23,5 +25,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('account.urls'),),
+    path('',views.index,name="index" ),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
